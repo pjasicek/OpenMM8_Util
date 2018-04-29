dcdc6967f95d4078b92ac207cafa4e17e0ffbf3e
pjasicek <pjasicek@gmail.com>
2018-04-29 12:38:03 +0200

    Init

---
+++ ColorRemover/ColorRemover/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ColorRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFolder = new System.Windows.Forms.Button();
+            this.folderPathTextbox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectFolder
+            // 
+            this.SelectFolder.Location = new System.Drawing.Point(200, 40);
+            this.SelectFolder.Name = "SelectFolder";
+            this.SelectFolder.Size = new System.Drawing.Size(34, 20);
+            this.SelectFolder.TabIndex = 0;
+            this.SelectFolder.Text = "...";
+            this.SelectFolder.UseVisualStyleBackColor = true;
+            this.SelectFolder.Click += new System.EventHandler(this.SelectFolder_Click);
+            // 
+            // folderPathTextbox
+            // 
+            this.folderPathTextbox.Location = new System.Drawing.Point(35, 40);
+            this.folderPathTextbox.Name = "folderPathTextbox";
+            this.folderPathTextbox.Size = new System.Drawing.Size(149, 20);
+            this.folderPathTextbox.TabIndex = 1;
+            this.folderPathTextbox.TextChanged += new System.EventHandler(this.folderPathTextbox_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(35, 123);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(199, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Directory with images (.bmp):";
+            // 
+            // GO
+            // 
+            this.GO.Location = new System.Drawing.Point(35, 75);
+            this.GO.Name = "GO";
+            this.GO.Size = new System.Drawing.Size(199, 33);
+            this.GO.TabIndex = 4;
+            this.GO.Text = "GO";
+            this.GO.UseVisualStyleBackColor = true;
+            this.GO.Click += new System.EventHandler(this.GO_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 173);
+            this.Controls.Add(this.GO);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.folderPathTextbox);
+            this.Controls.Add(this.SelectFolder);
+            this.Name = "Form1";
+            this.Text = "Color Remover";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFolder;
+        private System.Windows.Forms.TextBox folderPathTextbox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GO;
+    }
+}
+
---
+++ ColorRemover/ColorRemover/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorRemover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.folderPathTextbox.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void folderPathTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GO_Click(object sender, EventArgs e)
+        {
+            string[] bmpFiles = Directory.GetFiles(@"" + folderPathTextbox.Text, "*.bmp*", SearchOption.AllDirectories).ToArray();
+
+            int numFiles = bmpFiles.Length;
+
+            Console.WriteLine("Files: " + bmpFiles.ToString());
+            Console.WriteLine("Num files. " + numFiles);
+            Console.WriteLine("Path: " + folderPathTextbox.Text);
+
+            string outDir = folderPathTextbox.Text + "\\ColorRemover";
+            if (Directory.Exists(outDir))
+            {
+                Directory.Delete(outDir, true);
+            }
+            Directory.CreateDirectory(outDir);
+
+
+            int fileIdx = 0;
+            foreach (string file in bmpFiles)
+            {
+                Bitmap bmp = (Bitmap)Image.FromFile(file);
+                
+                //bmp.MakeTransparent(bmp.GetPixel(0, 0));
+                bmp.MakeTransparent(Color.FromArgb(0, 252, 252));
+                bmp.MakeTransparent(Color.FromArgb(0, 251, 252));
+                bmp.MakeTransparent(Color.FromArgb(0, 252, 251));
+                bmp.MakeTransparent(Color.FromArgb(0, 255, 255));
+                bmp.MakeTransparent(Color.FromArgb(0, 254, 255));
+                bmp.MakeTransparent(Color.FromArgb(252, 0, 252));
+                bmp.MakeTransparent(Color.FromArgb(252, 252, 0));
+                Image img = (Image)bmp;
+                string fname = Path.GetFileNameWithoutExtension(file);
+                img.Save(outDir + "\\" + fname + ".png", ImageFormat.Png);
+
+                fileIdx++;
+                progressBar1.Value = (fileIdx / numFiles) * 100;
+                progressBar1.Refresh();
+                Application.DoEvents();
+            }
+        }
+    }
+}
